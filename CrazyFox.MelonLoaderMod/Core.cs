ee5a927a638edd4177894d3685ac37215e72778f
Slimaeus <nguyenhongthai28042002@gmail.com>
2025-11-05 08:45:20 +0700

    Use OnSceneWasInitialized

--- CrazyFox.MelonLoaderMod/Core.cs
+++ CrazyFox.MelonLoaderMod/Core.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-[assembly: MelonInfo(typeof(CrazyFox.MelonLoaderMod.Core), "CrazyFox", "1.0.0", "Slimaeus", null)]
+[assembly: MelonInfo(typeof(CrazyFox.MelonLoaderMod.Core), "CrazyFox", "1.0.1", "Slimaeus", null)]
 [assembly: MelonGame("Ved", "Megabonk")]
 
 namespace CrazyFox.MelonLoaderMod;
@@ -17,7 +17,7 @@ public class Core : MelonMod
         LoggerInstance.Msg("Initialized.");
     }
 
-    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    public override void OnSceneWasInitialized(int buildIndex, string sceneName)
     {
         if (sceneName != _mainMenuSceneName)
             return;
@@ -32,20 +32,29 @@ public class Core : MelonMod
 
     private void UpdateFox()
     {
-        var characterKeys = DataManager.Instance.characterData.Keys;
         var weapons = new List<WeaponData>();
         var passives = new List<PassiveData>();
-        foreach (var key in characterKeys)
+        foreach (var unlockable in DataManager.Instance.unsortedUnlockables)
         {
-            var character = DataManager.Instance.characterData[key];
-
+            LoggerInstance.Msg($"Found unlockable: {unlockable.name} ({unlockable.WasCollected})");
+        }
+        foreach (var character in DataManager.Instance.unsortedCharacterData)
+        {
             weapons.Add(character.weapon);
             passives.Add(character.passive);
+            LoggerInstance.Msg($"Added {character.name} ({character.isEnabled})'s {character.weapon.name} ({character.weapon.isEnabled}) and {character.passive.name} ({character.passive}) ");
         }
         var fox = DataManager.Instance.characterData[ECharacter.Fox];
+        //var x = DataManager.Instance.characterData[ECharacter.Ninja];
 
+        //var baseWeapon = x.weapon;
+        //var basePassive = x.passive;
         fox.weapon = weapons[System.Random.Shared.Next(0, weapons.Count)];
         fox.passive = passives[System.Random.Shared.Next(0, passives.Count)];
+
+
+        //fox.weapon = baseWeapon;
+        //fox.passive = basePassive;
     }
 
 }
